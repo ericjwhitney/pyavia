5a42798f8daae350cc8ef07c4c1d3206c6c277e1
ericjwhitney <eric.j.whitney1@optusnet.com.au>
2019-12-13 10:47:00 +0800

    Substantially reworked to increase execution speed.

--- pyavia/compressible.py
+++ pyavia/compressible.py
@@ -7,7 +7,6 @@ Contains:
 # Last updated: 9 December 2019 by Eric J. Whitney
 
 from math import exp, log
-from typing import Set, Tuple
 
 from units import Dim, make_total_temp
 from util import iterate_fn, bisect_root
@@ -82,48 +81,66 @@ class ComprFlow:
 			ValueError on unknown gas.  TypeError on invalid arguments.
 		"""
 		self._M, self._w, self._gas, self._FAR = M, w, gas, FAR
-		if self._gas not in {'air', 'burned_kero', 'burned_diesel'}:
-			raise ValueError(f"Unknown gas: {gas}")
 
-		# Most other properties depend on T so this is set next.
-		self._P, self._T = 0, 0   # Dummies required for Tt / h functions.
+		# Most other properties depend on T so this is set next.  Also try to
+		# guess appropriate units system.
+		self._output_units = 'SI'
 		if T and not any([Tt, h, s]):
-			self._T = make_total_temp(T)  # Straightforward case.
+			# Straightforward case.
+			self._T = make_total_temp(T)
+			if self._T.units.θ[0] == '°R':
+				self._output_units = 'US'
 		elif Tt and not any([T, h, s]):
-			self._T = self._get_T_from_Tt(Tt)  # From stagnation.
+			# From stagnation.
+			_Tt = make_total_temp(Tt)
+			self._T = Dim(_get_T_from_Tt(_Tt.convert('K').value, self._M,
+			                         self._gas, self._FAR), _Tt.units)
+			if self._T.units.θ[0] == '°R':
+				self._output_units = 'US'
 		elif h and not any([T, Tt, s]):
-			self._T = self._get_T_from_h(h)  # From enthalpy.
+			# From enthalpy.
+			self._T = Dim(_get_T_from_h(h.convert('MJ/kg').value, self._gas,
+			                        self._FAR), 'K')
+			if h.units.θ[0] == '°R':
+				self._T.convert('°R')
 		elif s and not any([T, Tt, h]):
-			self._T = self._get_T_from_s(s)  # From entropy.
+			# From entropy.
+			self._T = Dim(_get_T_from_s(s.convert('kJ/kg/K').value, self._gas,
+			                        self._FAR), 'K')
+			if s.units.θ[0] == '°R':
+				self._T.convert('°R')
 		else:
 			raise TypeError(f"Invalid temperature arguments.")
 
 		# Precompute remaining properties.
-		if self._T.units.θ[0] == '°R':  # Try and match units intent.
-			self._output_units = 'US'
+		if self._output_units == 'US':
+			c_R_units = 'Btu/lbm/°R'
+			s_units = 'Btu/lbm/°R'
+			h_units = 'Btu/lbm'
+			a_units = 'ft/s'
 		else:
-			self._output_units = 'SI'
+			c_R_units = 'J/kg/K'
+			s_units = 'kJ/kg/K'
+			h_units = 'kJ/kg'
+			a_units = 'm/s'
 
 		T_K = self._T.convert('K').value
+		self._cp = Dim(1000 * _get_cp_from_T(T_K, self._gas, self._FAR),
+			               c_R_units)
+
 		try:
-			# Set via polynomial.
-			cp_, h_, s_ = _wfpoly_cp_h_s(T_K, self._gas, self._FAR)
-			self._cp = Dim(cp_*1000, 'J/kg/K')
-			self._h = Dim(h_*1000, 'kJ/kg')
-			self._s = Dim(s_, 'kJ/kg/K')
-
+			# These may not be settable, assign None in that case.
+			self._h = Dim(1000 * _get_h_from_T(T_K, self._gas, self._FAR),
+			              h_units)
+			self._s = Dim(_get_s_from_T(T_K, self._gas, self._FAR),
+			              s_units)
 		except ValueError:
-			if self._gas == 'air' and T_K > 2000:
-				# Set via vibrator model.
-				self._cp = Dim(_vib_cp(T_K)*1000, 'J/kg/K')
-				self._h, self._s = None, None
-			else:
-				raise ValueError(f"Temperature {self._T} outside range for "
-				                 f"gas '{self._gas}'")
+			self._h = None
+			self._s = None
 
-		self._set_R()
+		self._R = Dim(_get_R_from_T(T_K, self._gas, self._FAR), c_R_units)
 		self._γ = self._cp / (self._cp - self._R)
-		self._a = (self._γ * self._R * self._T)**0.5
+		self._a = ((self._γ * self._R * self._T)**0.5).convert(a_units)
 
 		if P and not Pt:
 			self._P = P
@@ -191,10 +208,7 @@ class ComprFlow:
 		"""Specific heat capacity at constant pressure of the gas."""
 		return self._cp
 
-	@property
-	def s(self) -> Dim:
-		"""Entropy.  Arbitrary zero basis."""
-		return self._s
+
 
 	@property
 	def cv(self) -> Dim:
@@ -203,12 +217,8 @@ class ComprFlow:
 
 	@property
 	def Q(self) -> Dim:
-		"""Enthalpy flowrate Q = h * w.  Returns kJ/s or Btu/s."""
-		_Q = self._h * self._w
-		if self._output_units == 'US':
-			return _Q.convert('Btu/s')
-		else:
-			return _Q.convert('kJ/s')
+		"""Enthalpy flowrate Q = h * w.  Inherits units from h."""
+		return self._h * self._w
 
 	@property
 	def FAR(self) -> float:
@@ -217,7 +227,7 @@ class ComprFlow:
 
 	@property
 	def gamma(self) -> float:
-		"""Ratio of specific heats γ = cp / c_v."""
+		"""Ratio of specific heats γ = cp / cv."""
 		return self._γ
 
 	@property
@@ -237,8 +247,7 @@ class ComprFlow:
 	@property
 	def Pt_on_P(self) -> float:
 		"""Ratio of total (stagnation) pressure to static pressure."""
-		return (1 + 0.5 * (self._γ - 1) * self._M ** 2) ** (self._γ / (
-				self._γ - 1))
+		return _Pt_on_P(self._γ, self._M)
 
 	@property
 	def Pt(self) -> Dim:
@@ -257,13 +266,13 @@ class ComprFlow:
 
 	@property
 	def s(self) -> Dim:
-		"""Entropy"""
+		"""Entropy.  Arbitrary zero basis."""
 		return self._s
 
 	@property
 	def Tt_on_T(self) -> float:
 		"""Ratio of total (stagnation) pressure to static temperature."""
-		return 1 + 0.5 * (self._γ - 1) * self._M ** 2
+		return _Tt_on_T(self._γ, self._M)
 
 	@property
 	def Tt(self) -> Dim:
@@ -280,108 +289,186 @@ class ComprFlow:
 		"""Mass flowrate."""
 		return self._w
 
-	# Internal methods --------------------------------------------------------
-
-	def _get_T_from_h(self, value: Dim) -> Dim:
-		"""Find the stream temperature of the gas given the specific
-		enthalpy, using a bisection method.  The initial T range is assumed
-		to be 200 K -> 2000 K."""
-
-		def h_error(T_try: Dim) -> Dim:
-			return self.replace(P=Dim(0, 'kPa'), T=T_try).h - value
-
-		T_L, T_R = Dim(200, 'K'), Dim(2000, 'K')
-		return bisect_root(h_error, T_L, T_R, max_its=50,
-		                   f_tol=Dim(1, 'J/kg'))  # Approx f_tol=1e-6
-
-	def _get_T_from_s(self, value: Dim) -> Dim:
-		"""Find the stream temperature of the gas given the specific
-		entropy, using a bisection method.  The initial T range is assumed
-		to be 200 K -> 2000K."""
-
-		def s_error(T_try: Dim) -> Dim:
-			return self.replace(P=Dim(0, 'kPa'), T=T_try).s - value
-
-		T_L, T_R = Dim(200, 'K'), Dim(2000, 'K')
-		return bisect_root(s_error, T_L, T_R, max_its=50,
-		                   f_tol=Dim(1e-4, 'J/kg/K'))
-
-	def _get_T_from_Tt(self, Tt_reqd: Dim) -> Dim:
-		"""Find the stream temperature of the gas given the total (stagnation)
-		temperature via iteration..  This is due to the non-linear
-		dependence of γ on Ts."""
-		Tt_reqd = make_total_temp(Tt_reqd)
-		if self._M == 0:  # Already at stagnation.
-			return Tt_reqd
-
-		# Iterate temperature due to non-linear dependence of γ on Ts.
-		# Start iteration with Ts = T.
-		def new_Ts(try_Ts):
-			flow = ComprFlow(P=Dim(0, 'kPa'), T=try_Ts, M=self._M, w=self._w,
-			                 gas=self._gas, FAR=self._FAR)
-			return Tt_reqd / flow.Tt_on_T
-
-		return iterate_fn(new_Ts, x_start=Tt_reqd, x_tol=Dim(1e-6, 'K'))
-
-	def _set_R(self) -> None:
-		R_air = 287.05287  # J/kg/K.
-		if self._gas == 'air':
-			R_adj = R_air
-		elif self._gas == 'burned_kero':
-			R_adj = R_air - 0.00990 * self.FAR + 1e-7 * self.FAR ** 2
-		elif self._gas == 'burned_diesel':
-			R_adj = R_air - 8.0262 * self.FAR + 3e-7 * self.FAR ** 2
-		else:
-			raise RuntimeError(f"Reached unreachable point.")
-
-		self._R = Dim(R_adj, 'J/kg/K')
-		if self._output_units == 'US':
-			self._R = self._R.convert('Btu/lbm/°R')
-
 
 # -----------------------------------------------------------------------------
-# Internal functions.
+
 
 # noinspection PyPep8Naming
-def _wfpoly_cp_h_s(T_K: float, gas: str, FAR: float) -> Tuple[float, float,
-                                                              float]:
+def _get_T_from_h(h: float, gas: str, FAR: float) -> float:
 	"""
-	Computes cp, h, s for a given temperature (K) using the interpolating
-	polynomial method of Walsh & Fletcher.  Valid for gas in {
-	'air', 'burned_kero', 'burned_diesel'}.  FAR is fuel-air ratio.
+	Computes stream temperature T [K] by bisecting between T = 200 K -> 2000 K
+	for a given h [MJ/kg].  Valid for gas 'air', 'burned_kero',
+	'burned_diesel'. FAR is fuel-air ratio.
+	"""
 
-	Returned values:
-		cp	kJ/kg/K
-		h 	MJ/kg
-		s	kJ/kg/K
+	# noinspection PyPep8Naming
+	def h_error(guess_T_K: float) -> float:
+		return _get_h_from_T(guess_T_K, gas, FAR) - h
 
+	return bisect_root(h_error, 200, 2000, max_its=50, f_tol=1e-6)
+
+
+# noinspection PyPep8Naming
+def _get_T_from_s(s: float, gas: str, FAR: float) -> float:
+	"""
+	Computes stream temperature T [K] by bisecting between T = 200 K -> 2000 K
+	for a given s [kJ/kg/K].  Valid for gas 'air', 'burned_kero',
+	'burned_diesel'. FAR is fuel-air ratio.
+	"""
+
+	# noinspection PyPep8Naming
+	def s_error(guess_T_K: float) -> float:
+		return _get_s_from_T(guess_T_K, gas, FAR) - s
+
+	return bisect_root(s_error, 200, 2000, max_its=50, f_tol=1e-6)
+
+
+# Walsh & Fletcher Eqn F3.23 coefficients.
+_A_COEFF = {
+	# Dry air with/without kerosene or diesel products of combustion.
+	'air': (0.992313, 0.236688, -1.852148, 6.083152, -8.893933, 7.097112,
+	        -3.234725, 0.794571, -0.081873, 0.422178, 0.001053), }
+
+# 'B' coefficients for corrections due to kerosene / diesel products of
+# combustion.
+_COEFF_B = (
+	-0.718874, 8.747481, -15.863157, 17.254096, -10.233795, 3.081778,
+	-0.361112, -0.003919, 0.0555930, -0.0016079)
+
+
+# noinspection PyPep8Naming
+def _get_cp_from_T(T_K: float, gas: str, FAR: float) -> float:
+	"""
+	Computes cp [kJ/kg/K] for a given temperature [K] using either the
+	interpolating polynomial method of Walsh & Fletcher ('air',
+	'burned_kero', 'burned_diesel') or a simple harmonic vibrator model
+	('air' only).  FAR is fuel-air ratio (polynomial method	only).
+
+	The simple harmonic	vibrator is per NACA TN 1135 Eqn 175, 176.  This
+	covers temperatures above the polynomial range limit of 2000 K.   In any
+	case the vibrator and polynomial model are within about 0.5% everywhere
+	under 2000 K.
+
+	Raises ValueError if cp cannot be computed.
+	"""
+	try:
+		# Polynomial model.
+		if not (200 <= T_K <= 2000):
+			raise ValueError
+		if gas in ('air', 'burned_kero', 'burned_diesel'):
+			coeff_a = _A_COEFF['air']
+		else:
+			raise ValueError
+
+		# Multiply out the polynomial, W&F Eqn F3.23.
+		Tz = T_K / 1000
+		cp = sum([a_i * Tz ** i for i, a_i in enumerate(coeff_a[0:9], 0)])
+
+		# Add FAR correction if required.
+		if gas in ('burned_kero', 'burned_diesel') and FAR > 0:
+			# W&F Eqn F3.24
+			cp += (FAR / (1 + FAR)) * sum(
+				[b_i * Tz ** i for i, b_i in enumerate(_COEFF_B[0:8], 0)])
+
+		return cp
+
+	except ValueError:
+		pass
+
+	try:
+		# Vibrator model.
+		if not gas == 'air' and T_K > 2000:
+			raise ValueError
+
+		c_p_perf = 1.0057  # American Meter. Society [kJ/kg/K]
+		γ_perf = 1.4
+		ratio = 5500 / (T_K * 9/5)  # Ratio (Theta) = 5,500°R / T [°R]
+		return c_p_perf * (1 + ((γ_perf - 1) / γ_perf) * (
+				(ratio ** 2) * exp(ratio) / (exp(ratio) - 1) ** 2))
+
+	except ValueError:
+		pass
+
+	raise ValueError(f"Cannot determine cp for gas '{gas}' at temperature "
+	                 f"{T_K}K")
+
+
+# noinspection PyPep8Naming
+def _get_h_from_T(T_K: float, gas: str, FAR: float) -> float:
+	"""
+	Computes h [MJ/kg] for a given temperature [K] using the interpolating
+	polynomial method of Walsh & Fletcher.  Valid for gas 'air',
+	'burned_kero', 'burned_diesel'.  FAR is fuel-air ratio.
+
+	Raises ValueError if h cannot be computed.
 	"""
 	# Applicable range.
 	if not (200 <= T_K <= 2000):
 		raise ValueError(f"T = {T_K} K outside allowable range.")
 
-	# Eqn F3.23 A coefficients.
-	A_COEFF = {
-		# Dry air with/without kerosene or diesel products of combustion.
-		'air': (0.992313, 0.236688, -1.852148, 6.083152, -8.893933, 7.097112,
-		        -3.234725, 0.794571, -0.081873, 0.422178, 0.001053), }
-
 	if gas in ('air', 'burned_kero', 'burned_diesel'):
-		coeff_a = A_COEFF['air']
+		coeff_a = _A_COEFF['air']
 	else:
 		raise ValueError(f"Unknown gas: {gas}")
 
-	# Multiply out the polynomials.
+	# Multiply out the polynomials, W&F Eqn F3.26
 	Tz = T_K / 1000
-
-	# Eqn F3.23
-	cp = sum([a_i * Tz ** i for i, a_i in enumerate(coeff_a[0:9], 0)])
-
-	# Eqn F3.26
 	h = coeff_a[9] + sum(
 		[(a_i / i) * Tz ** i for i, a_i in enumerate(coeff_a[0:9], 1)])
 
-	# Eqn F3.28
+	# Add FAR correction if required.
+	if gas in ('burned_kero', 'burned_diesel') and FAR > 0:
+		# W&F Eqn F3.27.
+		h += (FAR / (1 + FAR)) * (sum(
+			[(b_i / i) * Tz ** i for i, b_i in enumerate(_COEFF_B[0:7], 1)]) +
+		                          _COEFF_B[8])
+	return h
+
+
+# noinspection PyPep8Naming
+def _get_R_from_T(T_K: float, gas: str, FAR: float) -> float:
+	"""
+	Computes R [J/kg/K] for a given temperature [K] and FAR using the
+	method of
+	Walsh & Fletcher (200 <= T_K <= 200) or as a constant for air.
+
+	Raises ValueError if R cannot be computed.
+	"""
+	R_air = 287.05287  # J/kg/K.
+	if gas == 'air':
+		return R_air
+
+	if 200 <= T_K <= 2000:
+		if gas == 'burned_kero':
+			return R_air - 0.00990 * FAR + 1e-7 * FAR ** 2
+
+		if gas == 'burned_diesel':
+			return R_air - 8.0262 * FAR + 3e-7 * FAR ** 2
+
+	raise ValueError(f"Cannot determine R for gas '{gas}' at temperature "
+	                 f"{T_K} K")
+
+
+# noinspection PyPep8Naming
+def _get_s_from_T(T_K: float, gas: str, FAR: float) -> float:
+	"""
+	Computes s [kJ/kg/K] for a given temperature [K] using the interpolating
+	polynomial method of Walsh & Fletcher.  Valid for gas in 'air',
+	'burned_kero', 'burned_diesel'.  FAR is fuel-air ratio.
+
+	Raises ValueError if s cannot be computed.
+	"""
+	# Applicable range.
+	if not (200 <= T_K <= 2000):
+		raise ValueError(f"T = {T_K} K outside allowable range.")
+
+	if gas in ('air', 'burned_kero', 'burned_diesel'):
+		coeff_a = _A_COEFF['air']
+	else:
+		raise ValueError(f"Unknown gas: {gas}")
+
+	# Multiply out the polynomials, W&F Eqn F3.28 with EJ correction term.
+	Tz = T_K / 1000
 	EJW_s_A0_corr_term = coeff_a[0] * log(1000)
 	s = ((coeff_a[0] * log(Tz)) + sum(
 		[(a_i / i) * Tz ** i for i, a_i in enumerate(coeff_a[1:9], 1)]) +
@@ -389,43 +476,44 @@ def _wfpoly_cp_h_s(T_K: float, gas: str, FAR: float) -> Tuple[float, float,
 
 	# Add FAR correction if required.
 	if gas in ('burned_kero', 'burned_diesel') and FAR > 0:
-		# 'B' coefficients for corrections due to kerosene / diesel products of
-		# combustion.
-		coeff_b = (
-			-0.718874, 8.747481, -15.863157, 17.254096, -10.233795, 3.081778,
-		-0.361112, -0.003919, 0.0555930, -0.0016079)
-
-		# Eqn F3.24
-		cp += (FAR / (1 + FAR)) * sum(
-			[b_i * Tz ** i for i, b_i in enumerate(coeff_b[0:8], 0)])
-
-		# Eqn F3.27
-		h += (FAR / (1 + FAR)) * (sum(
-			[(b_i / i) * Tz ** i for i, b_i in enumerate(coeff_b[0:7], 1)]) +
-		                          coeff_b[8])
-
-		# Eqn F3.29
-		EJW_s_B0_corr_term = coeff_b[0] * log(1000)
-		# print(f"XXX TODO VERIFY FAR CORR TERM WITH EXAMPLES")
-		s += (FAR / (1 + FAR)) * (coeff_b[0] * log(Tz) + sum(
-			[(b_i / i) * Tz ** i for i, b_i in enumerate(coeff_b[1:8], 1)]) +
-		                          coeff_b[9] + EJW_s_B0_corr_term)
-
-	return cp, h, s
+		# W&F Eqn F3.29
+		EJW_s_B0_corr_term = _COEFF_B[0] * log(1000)
+		# XXX TODO VERIFY FAR CORR TERM WITH EXAMPLES
+		s += (FAR / (1 + FAR)) * (_COEFF_B[0] * log(Tz) + sum(
+			[(b_i / i) * Tz ** i for i, b_i in enumerate(_COEFF_B[1:8], 1)]) +
+		                          _COEFF_B[9] + EJW_s_B0_corr_term)
+	return s
 
 
-# noinspection NonAsciiCharacters,PyPep8Naming
-def _vib_cp(T_K: float) -> float:
-	"""Compute the cp value for air using a model of a simple  harmonic
-	vibrator, ref NACA TN 1135 Eqn 175, 176.  This covers temperatures above
-	the polynomial range limit of 2000 K.   In any case the vibrator and
-	polynomial model are within about 0.5% everywhere under 2000 K.
+# noinspection PyPep8Naming
+def _get_T_from_Tt(Tt_K: float, M: float, gas: str, FAR: float) -> float:
+	"""Return the stream temperature [K] of the gas given the total
+	(stagnation) temperature, Mach number, gas and FAR via iteration.  This is
+	due to the non-linear dependence of γ on Ts."""
+	if M == 0:  # Already at stagnation.
+		return Tt_K
 
-	Input value is K, returns cp in kJ/kg/K.
-	"""
-	c_p_perf = 1.0057  # American Meter. Society [kJ/kg/K]
-	γ_perf = 1.4
-	temp_R = Dim(T_K, 'K').convert('°R').value
-	ratio = 5500 / temp_R  # Ratio (Theta) = 5,500°R / T
-	return c_p_perf * (1 + ((γ_perf - 1) / γ_perf) * (
-			(ratio ** 2) * exp(ratio) / (exp(ratio) - 1) ** 2))
+	# Iterate temperature due to non-linear dependence of γ on Ts.
+	# Start iteration with Ts = T.
+	# noinspection PyPep8Naming
+	def new_Ts(guess_Ts):
+		cp = _get_cp_from_T(guess_Ts, gas, FAR)
+		R = _get_R_from_T(guess_Ts, gas, FAR)
+		γ = cp / (cp - R)
+		return Tt_K / _Tt_on_T(γ, M)
+
+	return iterate_fn(new_Ts, x_start=Tt_K, x_tol=1e-6)
+
+
+# noinspection PyPep8Naming
+def _Pt_on_P(γ: float, M: float) -> float:
+	"""Ratio of total (stagnation) pressure to static pressure at a
+	given γ and Mach number."""
+	return (1 + 0.5 * (γ - 1) * M ** 2) ** (γ / (γ - 1))
+
+
+# noinspection PyPep8Naming
+def _Tt_on_T(γ: float, M: float) -> float:
+	"""Ratio of total (stagnation) pressure to static temperature at a
+	given γ and Mach number."""
+	return 1 + 0.5 * (γ - 1) * M ** 2
