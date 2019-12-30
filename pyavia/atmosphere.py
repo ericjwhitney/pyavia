db4d158ba8335c90645fd81830050d2b7b95e5b5
eric.j.whitney <Amethyst2200>
2019-12-30 23:36:44 +0800

    Fix systemic typo.

--- pyavia/atmosphere.py
+++ pyavia/atmosphere.py
@@ -61,25 +61,25 @@ class Atmosphere:
 
         # Show some ISA SSL values.
         >>> atm = Atmosphere('SSL')
-        >>> print(func"P = {atm.pressure:.3f}, T = {atm.temperature:.2f}")
+        >>> print(f"P = {atm.pressure:.3f}, T = {atm.temperature:.2f}")
         P = 14.696 psi, T = 288.15 K
 
         # Show density for an ISA standard altitude (note that these are
         # formally geopotential altitudes).
         >>> atm = Atmosphere(H=Dim(10000, 'ft'))
-        >>> print(func"ρ = {atm.ρ:.3f}")
+        >>> print(f"ρ = {atm.ρ:.3f}")
         ρ = 0.905 kg/m³
 
         # Show the temperature ratio for a pressure altitude with a
         # temperature offset:
         >>> atm = Atmosphere(H_press=(34000,'ft'), T_offset=(+15,'Δ°C'))
-        >>> print(func"Theta = {atm.theta:.3f}")
+        >>> print(f"Theta = {atm.theta:.3f}")
         Theta = 0.818
 
         # Show the density ratio for an arbitrary non-standard atmosphere
         # based on temperature / pressure.
         >>> atm = Atmosphere(P=(90, 'kPa'), T=(-15, '°C'))
-        >>> print(func"σ = {atm.σ:.3f}")
+        >>> print(f"σ = {atm.σ:.3f}")
         σ = 0.991
     """
 
