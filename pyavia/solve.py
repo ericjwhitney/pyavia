cd93d75d228afc4aa59c0b52d29658b77cfefaef
ericjwhitney <eric.j.whitney1@optusnet.com.au>
2020-01-04 10:16:21 +0800

    Updated DQNM.

--- pyavia/solve.py
+++ pyavia/solve.py
@@ -9,6 +9,7 @@ Contains:
                     Quasi-Newton Merhod.
 """
 # Last updated: 30 December 2019 ny Eric J. Whitney
+from typing import Iterable
 
 import numpy as np
 
@@ -94,56 +95,77 @@ def bisect_root(func, x_a, x_b, maxits: int = 50, ftol=1e-6, verbose=False):
 
 def fixed_point(func, x0, xtol, relax=1.0, maxits: int = 15, verbose=False):
     """
-    Find the fixed point of a scalar function x = f(x) by iteratively
-    passing an estimate through the function.  Return when the point
-    stabilises.  Example:
+    Find the fixed point of a function x = f(x) by repeatedly passing x
+    through the function, and return when the result stabilises.  Here is an
+    example with a scalar function:
+
     >>> def f(x): return (x + 10) ** 0.25
-    >>> x_fixed = fixed_point(f, x0=-3, xtol=1e-6, verbose=True)
+    >>> x_scalar = fixed_point(f, x0=-3, xtol=1e-6, verbose=True)
     Fixed Point Iteration: x0 = -3
-    ... Iteration 1: x = 1.6265765616977852
-    ... Iteration 2: x = 1.8465580452920933
-    ... Iteration 3: x = 1.8552312312746646
-    ... Iteration 4: x = 1.855570704344644
-    ... Iteration 5: x = 1.8555839877110183
-    ... Iteration 6: x = 1.855584507474938
+    ... Iteration 1: x = [1.62657656]
+    ... Iteration 2: x = [1.84655805]
+    ... Iteration 3: x = [1.85523123]
+    ... Iteration 4: x = [1.8555707]
+    ... Iteration 5: x = [1.85558399]
+    ... Iteration 6: x = [1.85558451]
+    ... Converged.
+
+    This example uses the same function however x is now a list.  Note
+    that this only works because internally everything is converted to NumPy
+    arrays so that component-wise operations are valid and f() also returns an
+    array:
+
+    >>> x_vector = fixed_point(f, x0=[-3, -4], xtol=[1e-6]*2, verbose=True)
+    Fixed Point Iteration: x0 = [-3, -4]
+    ... Iteration 1: x = [1.62657656 1.56508458]
+    ... Iteration 2: x = [1.84655805 1.84411162]
+    ... Iteration 3: x = [1.85523123 1.85513544]
+    ... Iteration 4: x = [1.8555707  1.85556696]
+    ... Iteration 5: x = [1.85558399 1.85558384]
+    ... Iteration 6: x = [1.85558451 1.8555845 ]
     ... Converged.
 
     Args:
-        func: Function that returns a better estimate of 'x'.
-        x0: Starting value for 'x'. Any numeric type (including user
-            types) may be used.  Individual elements need not be the same type.
-        xtol: Stop when abs(x' - x) < xtol.
-        relax: Relaxation factor.  After the next trial value 'x_step' is
-            computed, it is revised to x' as follows:
-               x' = x + relax * (x_step - x)
-            If relax = 1 this is equivalent to standard iteration using x_step.
-            When relax < 1 (e.g. 0.5) this is an under-relaxation which can
-            add stability.
+        func: (Scalar / List) Function that returns a better estimate of x.
+        x0: (Scalar / List) Starting value for x. Any numeric type
+            including user types may be used, provided they support
+            component-wise mathematical operations.  Individual elements
+            need not be the same type.  Internally they are converted to
+            Numpy arrays.
+        xtol: (Scalar / List) Stop when abs(x' - x) < xtol.  The type/s or
+            element/s of xtol should correspond to x.
+        relax: (Float) Relaxation factor.  The next estimate of x is
+            computed as follows:
+               x' = (1 - relax) * x + relax * f(x)
+           If relax = 1 this is equivalent to the standard iteration of
+            x' = f(x). When relax < 1 (e.g. 0.5) this is an under-
+            relaxation which can add stability.
         maxits: (int) Iteration limit.
         verbose: (bool) If True, print iterations.
 
     Returns:
-        Converged x value.
+        (Scalar / List) Converged x value.
     """
     if verbose:
         print(f"Fixed Point Iteration: x0 = {x0}")
-    x, its = x0, 0
+    x = np.atleast_1d(x0)
+    its = 0
     while True:
         if its >= maxits:
             raise RuntimeError(f"Limit of {maxits} iterations exceeded.")
 
-        x_step = func(x)
-        x_next = x + relax * (x_step - x)
+        x_old = x
+        fx = np.atleast_1d(func(x) if len(x) > 1 else func(x.item()))
+        x = (1 - relax) * x + relax * fx
         its += 1
         if verbose:
-            print(f"... Iteration {its}: x = {x_next}")
+            print(f"... Iteration {its}: x = {x}")
 
-        if abs(x_next - x) < xtol:
+        if np.all(abs(x - x_old) < xtol):
             if verbose:
                 print(f"... Converged.")
             break
-        x = x_next
-    return x_next
+    return x.tolist() if len(x) > 1 else x.item()
 
 
 # ----------------------------------------------------------------------------
@@ -166,10 +188,12 @@ def solve_dqnm(func, x0, xtol=1e-6, ftol=None, bounds=None, maxits=25,
             of 0.9 the distance remaining to the boundary. In this way a
             solution on the boundary can stil be approached via a number of
             steps without the solver getting immediately stuck on the edge.
-        - EJW Addition: Check for extremely small moves where nu_0 approx
-            equals nu_1.  We drop back to first order for this step in this
-            case. 1e-20 is taken as a universally tiny number for the
-            comparison.
+        - EJW Addition: Check for extremely small moves where nu0 approx
+            equals nu1 i.e. abs(nu1 - nu0) < tiny.  We drop back to first
+            order for this step in this case.
+        - EJW Addition: Drops back to first order if ||Fx|| is escaping
+        upwards at this step with ||Fx'|| > 2*||Fx||.
+        - The universally tiny number is taken to be 1e-20 for both.
 
     Args:
         func: Vector valued function taking list-like x and returning
@@ -198,16 +222,23 @@ def solve_dqnm(func, x0, xtol=1e-6, ftol=None, bounds=None, maxits=25,
         ValueError for invalid parameters.
         RuntimeError if maximum iterations reached before convergence.
     """
+
+    def verbose_print(s):
+        if verbose:
+            print(s)
+
     if order != 1 and order != 2:
         raise ValueError(f"Order must be 1 or 2.")
 
     n = len(x0)
+    tiny = 1e-30
     if jacob_diag is None:
         d = np.ones(n)  # [D] Diagonal elements of Jacobian.  D = I.
     else:
         d = np.array(jacob_diag).astype(float)
     x = np.array(x0)
     fx = np.array(func(x))
+    fx_norm = np.linalg.norm(fx)
     s, y = None, None
     it = 1
 
@@ -218,10 +249,9 @@ def solve_dqnm(func, x0, xtol=1e-6, ftol=None, bounds=None, maxits=25,
         raise TypeError(f"Number of Jacobian diagonals ({len(d)}) does not "
                         f"match problem dimension ({n}).")
 
-    if verbose:
-        print(f"Diagonal Quasi-Newton Method - "
-              f"{'First' if order == 1 else 'Second'} Order - Solving {n} "
-              f"Equations:")
+    verbose_print(f"Diagonal Quasi-Newton Method - " +
+                  (f"First" if order == 1 else f"Second") + f" Order - " +
+                  f"Solving {n} Equations:")
 
     while True:
         if it >= maxits:
@@ -241,55 +271,73 @@ def solve_dqnm(func, x0, xtol=1e-6, ftol=None, bounds=None, maxits=25,
             mult = max(1.0, lo_mult, hi_mult)  # Factor stepped over bound.
             if mult > 1.0:
                 mult = 0.9 / mult
-                x = x_prev + mult * s
-                if verbose:
-                    print(f"*** Shortened step size by factor {mult:.5G} due "
-                          f"to boundary condition.")
+                if mult < tiny:
+                    raise RuntimeError(f"Resting on boundary, terminating.")
+                s *= mult
+                x = x_prev + s
+                verbose_print(f"*** Shortened step size x{mult:.3G} due to "
+                              f"boundary condition.")
 
         # Evaluate new point.
         fx = np.array(func(x))
+        fx_norm_prev = fx_norm
         fx_norm = np.linalg.norm(fx)
         y = fx - fx_prev
         it += 1
 
-        rho, mu, rho_norm = [None] * 3  # Reset for later calc.
-        if s_prev is not None and order == 2:
+        # Do second order update scheme if requested.
+        rho, mu, rho_norm = [None] * 3  # Reset as poss. trigger.
+        while (s_prev is not None) and (order == 2):
+            # Note:  ^^^ This is a 'while' masquerading as an 'if'.  There
+            # is a break at the end.
+
+            # EJW Addition: Drop back to first order if ||Fx|| is escaping.
+            if fx_norm > 2 * fx_norm_prev:
+                verbose_print(f"*** Skipped second order step: ||F(x)|| "
+                              f"rising. ")
+                break
+
             # Second order update scheme.
             nu1 = -np.linalg.norm(s)
             nu0 = -np.linalg.norm(s + s_prev)
 
             # EJW Addition: Check for extremely small moves.
-            if (nu1 - nu0) > 1e-20:
-                beta = -nu0 / (nu1 - nu0)
-                alpha = (beta ** 2) / (1 + 2 * beta)
-                rho = s - alpha * s_prev
-                mu = y - alpha * y_prev
-                rho_norm = np.linalg.norm(rho)
+            if abs(nu1 - nu0) < tiny:
+                verbose_print(f"*** Skipped second order step: ν₁ ≈ ν₂.")
+                break
 
-                if np.dot(rho, mu) < (xtol * rho_norm * np.linalg.norm(mu)):
-                    # Fallback to first order.
-                    rho = None
+            beta = -nu0 / (nu1 - nu0)
+            alpha = (beta ** 2) / (1 + 2 * beta)
+            rho = s - alpha * s_prev
+            mu = y - alpha * y_prev
+            rho_norm = np.linalg.norm(rho)
 
+            # Fallback to first order.
+            if np.dot(rho, mu) < (xtol * rho_norm * np.linalg.norm(mu)):
+                rho = None
+                verbose_print(f"*** Skipped second order step: "
+                              f"ρᵀμ < xₜₒₗ.||ρ||.||μ|| ")
+
+            break  # <<< Terminates the 'if' statement
+
+        # Do first order update scheme (as defined or as fallback).
         if rho is None:
-            # First order update scheme.
             rho, mu = s, y
             rho_norm = np.linalg.norm(rho)
 
-        if verbose:
-            if n < 10:
-                x_str = f", x* = " + f', '.join(f"{x_i:.5G}" for x_i in x)
-            else:
-                x_str = ''
-            print(f"... Iteration {it}: ||Fx|| = {fx_norm:.5G}, "
-                  f"||x' - x|| = {rho_norm:.5G}{x_str}")
+        if n < 10:
+            x_str = f", x* = " + f', '.join(f"{x_i:.5G}" for x_i in x)
+        else:
+            x_str = ''
+        verbose_print(f"... Iteration {it}: ||F(x)|| = {fx_norm:.5G}, "
+                      f"||x' - x|| = {rho_norm:.5G}{x_str}")
 
         # Check stopping criteria.
         if rho_norm <= xtol and (fx_norm <= ftol if ftol else True):
-            if verbose:
-                print(f"... Converged.")
+            verbose_print(f"... Converged.")
             return x.tolist()
 
         # Update [D].
-        if rho_norm >= xtol:  # tol:
+        if rho_norm >= xtol:
             g = rho ** 2
             d += g * (np.dot(rho, mu) - np.dot(g, d)) / (np.dot(g, g))
