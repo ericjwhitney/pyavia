9b0dec48342a3f48770bbd5d3ea67ccf7ccf0d62
ericjwhitney <eric.j.whitney1@optusnet.com.au>
2020-01-12 18:53:00 +0800

    Fixing doc build.

--- docs/conf.py
+++ docs/conf.py
@@ -48,11 +48,14 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_default_options = {'special-members': True,
+autodoc_default_options = {'members': True,  # EJW Fix?
+                           'special-members': True,
                            'exclude-members': '__dict__, __hash__, '
                                               '__weakref__, __module__, '
                                               '__slots__'
                            }
+
+print(f"EXCLUDE MEMBERS: {autodoc_default_options}")
 
 # -- Options for HTML output -------------------------------------------------
 
