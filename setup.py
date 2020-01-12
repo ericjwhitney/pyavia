a9c022899186bc658badb106bbebe9de25c8dfa0
ericjwhitney <eric.j.whitney1@optusnet.com.au>
2020-01-12 09:42:15 +0800

    Renamed.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyavia3",
+    name="pyavia",
     version="0.0.1",
     author="Eric J. Whitney",
     author_email="eric.j.whitney@optusnet.removethispart.com.au",
