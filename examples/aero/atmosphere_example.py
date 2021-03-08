9556fe452ba2d2d69eb50be59074d89a350fc6ec
ericjwhitney <eric.j.whitney1@optusnet.com.au>
2021-03-08 10:00:16 +0800

    General update, added Fortran functions.

--- examples/aero/atmosphere_example.py
+++ examples/aero/atmosphere_example.py
@@ -12,7 +12,7 @@ Atmosphere.set_default_style('SI')
 Atmosphere.unitdef_press = 'psi'
 
 # Show some ISA SSL values.
-atm = Atmosphere('SSL')
+atm = Atmosphere(H='SSL')
 print(f"P = {atm.pressure:.3f}, T = {atm.temperature:.2f}")
 # P = 101.325 kPa, T = 288.15 K
 
@@ -23,12 +23,12 @@ print(f"ρ = {atm.ρ:.3f}")
 # ρ = 0.905 kg/m³
 
 # Show the temperature ratio for a pressure altitude with a temperature offset:
-atm = Atmosphere(H_press=(34000, 'ft'), T_offset=(+15, 'Δ°C'))
+atm = Atmosphere(H_press=Dim(34000, 'ft'), T_offset=Dim(+15, 'Δ°C'))
 print(f"Theta = {atm.theta:.3f}")
 # Theta = 0.818
 
 # Show the density ratio for an arbitrary non-standard atmosphere based on
 # temperature / pressure.
-atm = Atmosphere(P=(90, 'kPa'), T=(-15, '°C'))
+atm = Atmosphere(P=Dim(90, 'kPa'), T=Dim(-15, '°C'))
 print(f"σ = {atm.σ:.3f}")
 # σ = 0.991
