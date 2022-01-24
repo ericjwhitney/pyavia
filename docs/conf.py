fe43ed9c846604b92b6e4645f98fc70d58228aa7
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2022-01-24 09:22:19 +0800

    Development version.

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2022, Eric J. Whitney'
 author = 'Eric J. Whitney'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
 
