78d37193571744359c9f61e875fe6dc50277e105
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2024-01-28 13:05:58 +0800

    Housekeeping prior to merge.

--- pyavia/data/__init__.py
+++ pyavia/data/__init__.py
@@ -2,6 +2,9 @@
 Functions relating to data manipulation, interpolation, filtering, et cetera.
 """
 
-from .filter import J211_2pole, J211_4pole, MovingAverage
+from .filter import (J211_2pole, J211_4pole, MovingAverage, pinv_simple,
+                     savgol_variable)
+from .function_1d import (Function1D, FitXY1D, Line1D, PCHIP1D,
+                          SmoothPoly1D)
 from .interpolate import (linear_int_ext, smooth_array2d, smooth_multi,
                           subd_num_list, Interp2Level)
