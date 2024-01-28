6f2c5cc430949ab192aba9d3e22d0c447ac30267
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2024-01-28 13:55:07 +0800

    Housekeeping.

--- pyavia/geometry/__init__.py
+++ pyavia/geometry/__init__.py
@@ -2,6 +2,8 @@
 Various aeronautic-specific geometry functions / objects.
 """
 
+# TODO Both 'data' and 'geometry' need to go into a more general mathematics
+#  library.
+
 from .bezier import bernstein_poly, bezier, bezier_deriv
-from .common import line_pt
-from .spacing import sine_spacing
+from .lines import line_pt
