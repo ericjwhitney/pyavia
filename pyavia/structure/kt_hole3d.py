a4ae0bc3497ea298a77932c8ccbd450b339701ba
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2024-01-28 14:17:29 +0800

    Housekeeping.

--- pyavia/structure/kt_hole3d.py
+++ pyavia/structure/kt_hole3d.py
@@ -1,42 +1,45 @@
-"""Computes three-dimensional stress-concentration factors for
+"""
+Computes three-dimensional stress-concentration factors for
 straight-shank or countersunk holes subjected to remote tension, remote
 bending, pin loading and wedge loading.
 
-Python version by Eric J. Whitney April 2020 based on FORTRAN code appearing in
-Shivakumar, K. N.  and Newman, J. C., "Stress Concentrations for
-Straight-Shank and Countersunk Holes in Plates Subjected to Tension,
-Bending, and Pin Loading", NASA Technical Paper NASA-TP-3192, June 1992."""
-
-# Last updated: 27 September 2020 by Eric J. Whitney
-
+Python version by Eric J. Whitney April 2020 based on FORTRAN code
+appearing in Shivakumar, K. N.  and Newman, J. C., "Stress
+Concentrations for Straight-Shank and Countersunk Holes in Plates
+Subjected  to Tension, Bending, and Pin Loading", NASA Technical Paper
+NASA-TP-3192, June 1992.
+"""
 import numpy as np
 
-# =============================================================================
+
+# Written by Eric J. Whitney, April 2020.
+
+# ======================================================================
 
 
 def kt_hole3d(rt: float, bt: float, zt: float, rw: float,
               lcase: str) -> float:
     r"""
-    Computes the three-dimensional stress concentration in countersunk and
-    straight shank rivet holes.  Original FORTRAN version developed by
-    Kunigal N. Shivakumar and J. C. Newman, Jr. April 1991.  Adapted to
-    Python by Eric J. Whitney, April 2020.
+    Computes the three-dimensional stress concentration in countersunk
+    and straight shank rivet holes.  Original FORTRAN version developed
+    by Kunigal N. Shivakumar and J. C. Newman, Jr. April 1991.  Adapted
+    to Python by Eric J. Whitney, April 2020.
 
-    .. note:: A countersink angle of :math:`\theta_c` = 100° is assumed by the
-       code which is typical for aircraft fasteners.  However as indicated
-       in NASA-TP-3192 page 5 the method is accurate to ±3.5% for
-       :math:`\theta_c` ± 10°, i.e. :math:`\theta_c` = 90° → 120°.
+    .. note:: A countersink angle of :math:`\theta_c` = 100° is assumed
+       by the code which is typical for aircraft fasteners.  However as
+       indicated in NASA-TP-3192 page 5 the method is accurate to ±3.5%
+       for :math:`\theta_c` ± 10°, i.e. :math:`\theta_c` = 90° → 120°.
 
     Parameters
     ----------
     rt : float
         Hole radius to thickness ratio, :math:`0.25 \leq r/t \leq 2.5`.
     bt : float
-        Straight shank length to plate thickness ratio, :math:`0 \leq b/t
-        \leq 1`.  If ``bt == 1`` the hole is straight shank only.
+        Straight shank length to plate thickness ratio, :math:`0 \leq
+         b/t \leq 1`.  If ``bt == 1`` the hole is straight shank only.
     zt : float
-        Location where stress concentration factor is required as a ratio of
-        plate thickness, :math:`-0.5 \leq z/t \leq 0.5`.
+        Location where stress concentration factor is required as a
+        ratio of plate thickness, :math:`-0.5 \leq z/t \leq 0.5`.
         .. note: `z` is measured from the mid-plane of the plate.
     rw : float
         Hole radius to plate half-width, :math:`r/w < 0.25`.
@@ -60,17 +63,17 @@ def kt_hole3d(rt: float, bt: float, zt: float, rw: float,
 
             - Remote tension: :math:`K_t=\sigma_{max}/\sigma_t` where
               :math:`\sigma_t` is the remote applied stress.
-            - Remote bending: :math:`K_b=\sigma_{max}/[6M/(t^2)]` where `M` =
-              remote applied moment per unit width.
-            - Pin (or rivet) load: :math:`K_p=\sigma_{max}/[P/(2rt)]` where
-              `P` = pin load.
-            - Wedge load: :math:`K_w=\sigma_{max}/[P/(2rt)]` where `P` = wedge
-              load
+            - Remote bending: :math:`K_b=\sigma_{max}/[6M/(t^2)]` where
+              `M` = remote applied moment per unit width.
+            - Pin (or rivet) load: :math:`K_p=\sigma_{max}/[P/(2rt)]`
+              where `P` = pin load.
+            - Wedge load: :math:`K_w=\sigma_{max}/[P/(2rt)]` where `P`
+              = wedge load
 
-            .. note:: `K` / SCF for simulated rivet loading is obtained by
-               adding one-half of the SCF for remote tension S = p/(2wt) and
-               one-half of the SCF for wedge loading (`2w` is total width of
-               plate).
+            .. note:: `K` / SCF for simulated rivet loading is obtained
+               by adding one-half of the SCF for remote tension S =
+               p/(2wt) and one-half of the SCF for wedge loading (`2w`
+               is total width of plate).
     """
     if not (0.0 <= bt <= 1.0):
         raise ValueError(f"b/t = {bt:.3f} out of range (0..1).")
@@ -101,7 +104,7 @@ def kt_hole3d(rt: float, bt: float, zt: float, rw: float,
         return scf1 + (scf2 - scf1) / 0.25 * (bt - z_lwr)
 
 
-# =============================================================================
+# ======================================================================
 # Straight shank constants and function.
 
 _SS_ALP = np.reshape([
@@ -115,6 +118,7 @@ _SS_ALP = np.reshape([
     -1.5767, 3.0242, -2.0075, 0.4169,
     3.1870, -6.5555, 4.4847, -0.9450,
     -2.3673, 4.6981, -3.1644, 0.6614], (4, 5, 2), order='F')
+
 _SS_ALPB = np.reshape([
     3.1773, -1.7469, 0.9801, -0.1875,
     -0.2924, 0.1503, -0.0395, 0.0040,
@@ -122,10 +126,12 @@ _SS_ALPB = np.reshape([
     -1.2427, 2.7202, -1.8804, 0.3957], (4, 4), order='F')
 
 
+# ----------------------------------------------------------------------
+
 def _kt_hole_straight(rt, zt, lcase):
     """
-    Three-dimensional stress concentration equation for straight shank rivet
-    hole subjected to:
+    Three-dimensional stress concentration equation for straight shank
+    rivet hole subjected to:
         - Remote tension.
         - Remote bending.
         - Pin loading in hole (r/w < 0.25).
@@ -154,7 +160,7 @@ def _kt_hole_straight(rt, zt, lcase):
     return scf
 
 
-# =============================================================================
+# ======================================================================
 # Countersunk constants and function.
 
 _CS_ALP = np.reshape([
@@ -198,6 +204,8 @@ _CS_BET = np.reshape([
     0.4036, 0.1992, 0.8738, -0.3866], (3, 5, 4, 2), order='F')
 
 
+# ----------------------------------------------------------------------
+
 def _kt_hole_csunk(rt, k, zt, lcase):
     """
     Three-dimensional stress concentration factor for countersunk rivet
@@ -206,9 +214,9 @@ def _kt_hole_csunk(rt, k, zt, lcase):
         - Remote bending.
 
     Solution is for the countersink angle of 100 degrees and a selected
-    value of k = 0, 1, 2 or 3 corresponding to b/t = 0, 0.25, 0.50 or 0.75.
-    Results for other b/t values are computed by linear interpolation
-    between the bracketing b/t values.
+    value of k = 0, 1, 2 or 3 corresponding to b/t = 0, 0.25, 0.50 or
+    0.75.  Results for other b/t values are computed by linear
+    interpolation between the bracketing b/t values.
 
     Range of parameters:
         - -0.5 < z/t < 0.5.
@@ -250,7 +258,7 @@ def _kt_hole_csunk(rt, k, zt, lcase):
     return scf
 
 
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 def _nzt(bt, zt, bt1, bt2):
     """Evaluate appropriate z-location for countersunk hole."""
@@ -261,3 +269,5 @@ def _nzt(bt, zt, bt1, bt2):
         zt1 = bt1 - 0.5 + (zt - bt + 0.5) * bt1 / bt
         zt2 = bt2 - 0.5 + (zt - bt + 0.5) * bt2 / bt
     return zt1, zt2
+
+# ======================================================================
