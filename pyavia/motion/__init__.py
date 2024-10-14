598f820ce21b40ed87aed75b56cfb2561291f239
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2024-10-14 10:06:45 +0800

    Delete pyavia/motion directory

--- pyavia/motion/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This sub-package relates to dynamics motion in flight, control, landing
-and other related topics.
-"""
-
-from .ballistic import (BallisticTrajectory, ballistic,
-                        ballistic_variable)
-from .landing import LandingImpact, landing_energy
