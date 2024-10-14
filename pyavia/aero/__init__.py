73992d399088483e9d21cdd464cbbe70f4c933dc
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2024-10-14 10:02:07 +0800

    Delete pyavia/aero directory

--- pyavia/aero/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Functions relating to aerodynamics, thermodynamics and fluid dynamics.
-"""
-
-from .atmosphere import (Atmosphere, geo_alt_to_pot, pot_alt_to_geo)
-from .gas import Gas
-from .perfect_gas import PerfectGas
-from .imperfect_gas import ImperfectGas
