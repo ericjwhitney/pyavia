30d33e313cc84f6c56a98a2b5a7f99a24ec2a8a5
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2024-10-14 10:04:34 +0800

    Delete pyavia/geometry directory

--- pyavia/geometry/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Various aeronautic-specific geometry functions / objects.
-"""
-
-# TODO Both 'data' and 'geometry' need to go into a more general mathematics
-#  library.
-
-from .bezier import bernstein_poly, bezier, bezier_deriv
-from .lines import line_pt
