1173c81e2bf11d75a2fd809ff9f95d177d239ed7
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2026-09-24 16:47:06 +0800

    Updated numeric examples.

--- examples/numeric/solve/dqnm_example.py
+++ examples/numeric/solve/dqnm_example.py
@@ -1,13 +1,16 @@
 #!usr/bin/env python3
-
-# Examples of the solution of systems of equations.
-# Last updated: 21 December 2022 by Eric J. Whitney
-
+"""
+Examples of the solution of systems of equations.
+"""
 
 import numpy as np
 
-from pyavia.solve import solve_dqnm
+from pyavia.numeric.solve import solve_dqnm
 
+
+# Written by Eric J. Whitney. Last updated December 2022.
+
+# ======================================================================
 
 def linear_system_example(x):
     """A simple linear system of equations."""
@@ -19,10 +22,14 @@ def linear_system_example(x):
     return res
 
 
+# ----------------------------------------------------------------------
+
 def std_problem_1(x):
     """Standard start point x0 = (0.87, 0.87, ...)"""
     return [np.cos(x_i) - 1 for x_i in x]
 
+
+# ----------------------------------------------------------------------
 
 def std_problem_3(x):
     """Standard start point x0 = (0.5, 0.5, ...)"""
@@ -34,6 +41,8 @@ def std_problem_3(x):
     return f
 
 
+# ======================================================================
+
 # Solve one of the above problems at a given size.
 ndim = 500
 x0 = [0.5] * ndim
@@ -41,6 +50,10 @@ bounds = ([-1] * ndim, [+np.inf] * ndim)
 x_result = solve_dqnm(std_problem_1, x0=x0, ftol=1e-5, xtol=1e-6,
                       bounds=bounds, maxits=50, order=2, verbose=True)
 
-print("\nResult x = " +
-      np.array2string(np.asarray(x_result), precision=6, suppress_small=True,
-                      separator=', ', sign=' ', floatmode='fixed'))
+print(
+    "\nResult x = " +
+    np.array2string(
+        np.asarray(x_result), precision=6, suppress_small=True,
+        separator=', ', sign=' ', floatmode='fixed'
+    )
+)
