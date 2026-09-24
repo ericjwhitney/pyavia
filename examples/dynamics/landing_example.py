1499d181073c75b3037c8608f092d6a938a63ef6
Eric J. Whitney <58320720+ericjwhitney@users.noreply.github.com>
2026-09-24 16:54:21 +0800

    Updated examples.

--- examples/dynamics/landing_example.py
+++ examples/dynamics/landing_example.py
@@ -1,20 +1,27 @@
-from pyavia.dynamics.landing import landing_energy
+"""
+Compute landing load factor and deflection using conservation of energy.
+
+"""
+from pyavia.dynamics import landing_energy
 from pyavia.dynamics.tests.test_landing import _tyre_ft_lbf
 
 # Written by Eric J. Whitney, November 2023.
 
-# Compute landing load factor and deflection using conservation of energy.
+# ======================================================================
+
 # Note the following in the mass values below:
 #   - [slug] is used as the consistent mass unit.
 #   - Aircraft mass is divided by 2 for per-leg analysis.
 
-result = landing_energy(strut=2158.3,  # Strut spring K [lbf/ft]
-                        tyre=_tyre_ft_lbf,  # Tabulated (ft, lbf)
-                        g=32.174,  # [ft/s^2]
-                        K=2 / 3,  # L/W for light aircraft
-                        m_ac=1200 * 0.031081 / 2,  # lbm -> [slug]
-                        m_t=5.0 * 0.031081,  # lbm -> [slug]
-                        V_sink=7.0)  # [fps]
+result = landing_energy(
+    strut  = 2158.3,              # Strut spring K [lbf/ft]
+    tyre   = _tyre_ft_lbf,        # Tabulated (ft, lbf)
+    g      = 32.174,              # [ft/s^2]
+    K      = 2 / 3,               # L/W for light aircraft
+    m_ac   = 1200 * 0.031081 / 2, # lbm -> [slug]
+    m_t    = 5.0 * 0.031081,      # lbm -> [slug]
+    V_sink = 7.0                  # [fps]
+)
 
 print(f"Total aircraft weight = {result.W_ac:.1f} lbf, "
       f"lift = {result.L:.1f} lbf")
